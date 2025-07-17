e72b6c664b7abf5e0dc9dac722120e1132035d1a
nizarafham <nizarafham17@gmail.com>
2025-07-17 21:38:23 +0700

    add ApiResponder trait for consistent JSON responses n seeding admin users

---
+++ app/Http/Traits/ApiResponder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Traits;
+
+trait ApiResponder
+{
+    protected function success($data = null, $message = "success", $statusCode = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $statusCode);
+    }
+
+    protected function error($message = "error", $statusCode = 400, $data = null)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'data' => $data
+        ], $statusCode);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,24 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '08123456789',
+            'password' => Hash::make('admin')
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            DivisionSeeder::class,
         ]);
     }
 }
